f069fe90e413995bf6e12ad67d2b63383aa96d50
ANUPAMA DHIR <sabhssabhs@yahoo.com>
2020-04-22 11:37:43 +0800

    Added code to add multiple items to cart

--- src/test/java/pages/CartObjects.java
+++ src/test/java/pages/CartObjects.java
@@ -6,20 +6,26 @@ import org.openqa.selenium.WebElement;
 
 public class CartObjects {
 	private static WebDriver driver = null;
+	private static By product_select = null;
+	private static WebElement element = null;
 	By allproducts_select = By.cssSelector("h4.product-name");
-	By product_select = By.xpath("//*[contains(text(),'Cucumber')]/ancestor::div[@class='product']");
+//	By product_select = By.xpath("//*[contains(text(),'Cucumber')]/ancestor::div[@class='product']");
 	
 	public CartObjects(WebDriver driver) {
 		this.driver = driver;
+		
 	}
-	public void findItem() {
-		WebElement element =driver.findElement(product_select);
+	public void findItem(String[] itemsNeeded) {
+		for (String itemToAdd: itemsNeeded) {
+		product_select = By.xpath("//*[contains(text(),'"+itemToAdd+"')]/ancestor::div[@class='product']");
+		element =driver.findElement(product_select);
 		element.findElement(By.xpath(".//div[@class='product-action']/button")).click();
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
 		}
 	}
 }
--- src/test/java/test/CartObjectsTest.java
+++ src/test/java/test/CartObjectsTest.java
@@ -8,23 +8,24 @@ import pages.CartObjects;
 
 public class CartObjectsTest {
 	private static WebDriver driver = null;
+	private static String[] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot"};
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		setUp();
 		findItemTest();
-		tearDown();
+//		tearDown();
 
 	}
 	public static void setUp() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
-	//	driver.manage().window().maximize();
+		//	driver.manage().window().maximize();
 	}
 
 	public static void findItemTest() {
 		CartObjects cartObj = new CartObjects(driver);
-		cartObj.findItem();
+		cartObj.findItem(itemsNeeded);
 	}
 	public static void tearDown() {
 		driver.close();
