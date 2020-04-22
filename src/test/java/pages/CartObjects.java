3744eeee10280f886a69ec8d27d1158ce2574609
ANUPAMA DHIR <sabhssabhs@yahoo.com>
2020-04-22 15:56:27 +0800

    Added code for explicit wait

--- src/test/java/pages/CartObjects.java
+++ src/test/java/pages/CartObjects.java
@@ -3,13 +3,19 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CartObjects {
 	private static WebDriver driver = null;
 	private static By product_select = null;
 	private static WebElement element = null;
-	By allproducts_select = By.cssSelector("h4.product-name");
-//	By product_select = By.xpath("//*[contains(text(),'Cucumber')]/ancestor::div[@class='product']");
+	private static WebDriverWait w= null;
+	By cart_click = By.cssSelector("img[alt='Cart']");
+	By checkout_click = By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]");
+	By promocode_enter = By.cssSelector("input.promoCode");
+	By applyPromo_click = By.cssSelector("button.promoBtn");
+	By promoApply_check = By.cssSelector("span.promoInfo");
 	
 	public CartObjects(WebDriver driver) {
 		this.driver = driver;
@@ -20,12 +26,19 @@ public class CartObjects {
 		product_select = By.xpath("//*[contains(text(),'"+itemToAdd+"')]/ancestor::div[@class='product']");
 		element =driver.findElement(product_select);
 		element.findElement(By.xpath(".//div[@class='product-action']/button")).click();
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		
 		}
-		}
+	}
+	
+	public void checkout() {
+		w= new WebDriverWait(driver, 5);
+		driver.findElement(cart_click).click();
+		driver.findElement(checkout_click).click();
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
+		driver.findElement(promocode_enter).sendKeys("rahulshettyacademy");
+		driver.findElement(applyPromo_click).click();
+		
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+		System.out.println(driver.findElement(promoApply_check).getText());
 	}
 }
--- src/test/java/test/CartObjectsTest.java
+++ src/test/java/test/CartObjectsTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,12 +15,14 @@ public class CartObjectsTest {
 		// TODO Auto-generated method stub
 		setUp();
 		findItemTest();
+		checkoutTest();
 //		tearDown();
 
 	}
 	public static void setUp() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
+	//	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 		//	driver.manage().window().maximize();
 	}
@@ -26,6 +30,11 @@ public class CartObjectsTest {
 	public static void findItemTest() {
 		CartObjects cartObj = new CartObjects(driver);
 		cartObj.findItem(itemsNeeded);
+	}
+	
+	public static void checkoutTest() {
+		CartObjects cartObj = new CartObjects(driver);
+		cartObj.checkout();
 	}
 	public static void tearDown() {
 		driver.close();
